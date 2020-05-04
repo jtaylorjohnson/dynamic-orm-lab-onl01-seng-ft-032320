5e973178fd2acf583e578ea23b70f809fe75eb22
James Johnson <jtaylorjohnson@gmail.com>
2020-05-04 00:54:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,4 +53,9 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, attribute)
+  end
+  
 end
